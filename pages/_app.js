65c866f1ee7d8e9c96c873fe8b71246febce5071
Deepak <deepakdash137@gmail.com>
2023-11-12 12:02:15 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import NavBar from '../components/NavBar';
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  <NavBar />
+  return <Component {...pageProps} />
+}
+
+export default MyApp
